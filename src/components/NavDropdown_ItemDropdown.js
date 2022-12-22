8a7424e48acee900e47e63820e362b7837ba783b
Edwin Donaldo Saucedo Vazquez <edwinsaucedomx@EdwinSaucedoMX>
2022-12-22 15:35:27 -0600

    Fixing Navbar Appearance

--- src/components/NavDropdown_ItemDropdown.js
+++ src/components/NavDropdown_ItemDropdown.js
@@ -5,14 +5,15 @@ function NavDropdown_ItemDropdown() {
     return (
         <div>
             <NavDropdown title="Hello">
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <Accordion>
-                    <Accordion.Item>
+                <Accordion bsPrefix='accordion accordion-nav'>
+                    <Accordion.Item >
                         <Accordion.Header>Accordion Item #1</Accordion.Header>
                         <Accordion.Body>
+                            
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            
                         </Accordion.Body>
                     </Accordion.Item>
                 </Accordion>
